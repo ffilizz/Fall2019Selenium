abfeb5011f1e01a31ca2e7d3bb09442665dcc036
ffilizz <filizikibas@gmail.com>
2020-03-01 14:23:52 -0600

    create basic navigation class update

--- src/test/java/com/automation/tests/day2/BasicNavigation.java
+++ src/test/java/com/automation/tests/day2/BasicNavigation.java
@@ -7,26 +7,53 @@ import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import org.openqa.selenium.safari.SafariDriver;
 public class BasicNavigation {
-    public static void main(String[] args) throws Exception{
+    public static void main(String[] args) throws Exception {
         //to start selenium script we need:
         //setup webdriver (browser driver) and create webdriver object
-        WebDriverManager.chromedriver().setup();
+        WebDriverManager.chromedriver().version("79.0").setup();
         WebDriver driver = new ChromeDriver();
         //In selenium, everything starts from WebDriver interface'
         //ChromeDriver extends RemoteWebDriver --> implements WebDriver
         driver.get("http://google.com");//to open a website
+//        driver.manage().window().maximize();
+//        driver.manage().window().fullscreen();
         Thread.sleep(3000);//for demo, wait 3 seconds
         //method that return page title
         //you can also see it as tab name, in the browser
         String title = driver.getTitle();//returns <title>Some title</title> text
-        String expectedTitle="Google";
-        System.out.println("Title is..."+title);
-        if(expectedTitle.equals(title)){
+        String expectedTitle = "Google";
+        System.out.println("Title is..." + title);
+        if (expectedTitle.equals(title)) {
             System.out.println("Test PASSED!");
-        }else{
+        } else {
             System.out.println("TEST FAILED!");
         }
+        driver.navigate().to("http://amazon.com");
+        if (driver.getTitle().toLowerCase().contains("amazon")) {
+            System.out.println("TEST PASSED");
+        } else {
+            System.out.println("TEST FAILED!");
+
+        }
+        // come back to google
+        driver.navigate().back();
+
+
+        driver.close();//to close browser
+        //browser cannot close itself
+        verifyEquals(driver.getTitle(), "Google");
+        //must be at the end
         driver.close();//to close browser
         //browser cannot close itself
     }
+
+    public static void verifyEquals(String arg1, String arg2) {
+        if (arg1.equals(arg2)) {
+            System.out.println("TEST PASSED!");
+        } else {
+            System.out.println("TEST FAILED");
+        }
+
+    }
 }
+
