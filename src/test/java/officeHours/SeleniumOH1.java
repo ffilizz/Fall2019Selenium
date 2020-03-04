b04bb1d17ec23499899055f2bb0fa4058715a343
ffilizz <filizikibas@gmail.com>
2020-03-03 19:33:33 -0600

    create basic navigation class update

---
+++ src/test/java/officeHours/SeleniumOH1.java
@@ -0,0 +1,19 @@
+package officeHours;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
+public class SeleniumOH1 {
+
+    public static void main(String[] args) {
+
+        WebDriverManager.chromedriver().version("79.0").setup();
+
+
+
+
+    }
+}
