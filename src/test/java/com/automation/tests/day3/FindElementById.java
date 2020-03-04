a21e23df7b362ca8dbb1805ba70b8b6334b86a9d
ffilizz <filizikibas@gmail.com>
2020-03-03 20:30:58 -0600

    Finding element By.id(); and logout button By.linkText();

--- src/test/java/com/automation/tests/day3/FindElementById.java
+++ src/test/java/com/automation/tests/day3/FindElementById.java
@@ -4,6 +4,7 @@ import com.automation.utilities.DriverFactory;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class FindElementById {
@@ -29,6 +30,16 @@ public class FindElementById {
         }else{
             System.out.println("TEST FAILED!");
         }
+
+        //let's click on Logout button. It looks like a button, but it's actually a link
+        //every element with <a> tag is a link
+        //if you have couple spaces in the text, just use partialLinkText instead of linkText
+        //linkText - equals()
+        //partialLinkText - contains()
+
+        WebElement logout=driver.findElement(By.linkText("Logout"));
+        logout.click();
+        Thread.sleep(3000);
 
         driver.quit();
     }
