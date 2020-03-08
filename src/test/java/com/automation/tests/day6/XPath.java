38934999c143d6477d45cfb007f5e3af29faa6f0
ffilizz <filizikibas@gmail.com>
2020-03-08 16:35:35 -0500

    Xpath

--- src/test/java/com/automation/tests/day6/XPath.java
+++ src/test/java/com/automation/tests/day6/XPath.java
@@ -17,6 +17,23 @@ public class XPath {
         WebElement result= driver.findElement(By.id("result"));
         System.out.println("result = " + result.getText());
 
+        //click on button #2
+        WebElement btn2 = driver.findElement(By.xpath("//button[text()='Button 2']"));
+        btn2.click();
+        WebElement btn3 = driver.findElement(By.xpath("//button[starts-with(@id,'button_')]"));
+        btn3.click();
+        System.out.println(result.getText());
+
+        WebElement btn4 = driver.findElement(By.id("//button[contains(@id,'_button')][1]"));
+        btn4.click();
+        System.out.println(result.getText());
+
+        WebElement btn5 = driver.findElement(By.xpath("//button[contains(text(),'5')]"));
+        btn5.click();
+        System.out.println(result.getText());
+
+
+
         BrowserUtils.wait(3);
         driver.quit();
 
