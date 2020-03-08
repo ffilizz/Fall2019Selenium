880c68f5a23b752d44cff916b3a1a72d76fb3be7
ffilizz <filizikibas@gmail.com>
2020-03-08 11:05:57 -0500

    Select by Text

---
+++ src/test/java/com/automation/tests/day6/SelectByText.java
@@ -0,0 +1,51 @@
+package com.automation.tests.day6;
+
+import com.automation.utilities.BrowserUtils;
+import com.automation.utilities.DriverFactory;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
+
+import java.util.List;
+
+public class SelectByText {
+
+    public static void main(String[] args) {
+
+        WebDriver driver = DriverFactory.createDriver("chrome");
+        driver.get("http://practice.cybertekschool.com/dropdown");
+
+        BrowserUtils.wait(3);
+        //create a webelement object for drop-down first
+        WebElement simpleDropdown = driver.findElement(By.id("dropdown"));
+        //provide weblement object into constructor
+        Select selectSimpleDropdown = new Select(simpleDropdown);
+        //select by visible text
+        selectSimpleDropdown.selectByVisibleText("Option 2");
+        BrowserUtils.wait(3);
+
+        selectSimpleDropdown.selectByVisibleText("Option 1");
+
+        //select you DOB
+        Select selectYear = new Select(driver.findElement(By.id("year")));
+        Select selectMonth = new Select(driver.findElement(By.id("month")));
+        Select selectDay = new Select(driver.findElement(By.id("day")));
+
+        selectYear.selectByVisibleText("2003");
+        selectMonth.selectByVisibleText("February");
+        selectDay.selectByVisibleText("25");
+        BrowserUtils.wait(3);
+
+        List<WebElement> months=selectMonth.getOptions();
+        for(WebElement eachMonth : months){
+            String monthName=eachMonth.getText();
+            selectMonth.selectByVisibleText(eachMonth.getText());
+            BrowserUtils.wait(1);
+
+        }
+
+        BrowserUtils.wait(3);
+        driver.quit();
+    }
+}
