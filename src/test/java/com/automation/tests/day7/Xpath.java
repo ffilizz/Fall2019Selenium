95450c7d4862801097fcda72c7aa0fff0fb963f5
ffilizz <filizikibas@gmail.com>
2020-03-10 20:05:49 -0500

    Xpath practice

---
+++ src/test/java/com/automation/tests/day7/Xpath.java
@@ -0,0 +1,35 @@
+package com.automation.tests.day7;
+
+import com.automation.utilities.BrowserUtils;
+import com.automation.utilities.DriverFactory;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class Xpath {
+    static String userNameLocator = "//label[text()='Username']/following-sibling::input";
+    static String passwordLocator = "//label[text()='Password']/following-sibling::input";
+    static String loginLocator="//button[contains(text(), 'Login')]";
+    public static void main(String[] args) {
+
+        WebDriver driver= DriverFactory.createDriver("chrome");
+        driver.get("http://practice.cybertekschool.com/login");
+        BrowserUtils.wait(3);
+        driver.findElement(By.xpath(userNameLocator)).sendKeys("tomsmith");
+        driver.findElement(By.xpath(passwordLocator)).sendKeys("SuperSecretPassword");
+        driver.findElement(By.xpath(loginLocator)).click();
+
+
+
+
+
+
+
+
+
+
+    BrowserUtils.wait(3);
+    driver.quit();
+
+
+    }
+}
