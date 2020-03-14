3c13af26c28133775ac6d1a8c4ea67aa40a273ae
ffilizz <filizikibas@gmail.com>
2020-03-14 15:43:45 -0500

    Practice test

--- src/test/java/com/automation/tests/day8/PracticeTests.java
+++ src/test/java/com/automation/tests/day8/PracticeTests.java
@@ -6,6 +6,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.Assert;
 import org.testng.annotations.*;
 
@@ -32,7 +33,13 @@ public class PracticeTests {
     @BeforeMethod
     public void setUp() {
         WebDriverManager.chromedriver().version("79").setup();
-        driver = new ChromeDriver();
+        //INTERVIEW QUESTION: HOW TO HANDLE SSL ISSUES IN SELENIUM?
+        //ChromeOptions - use to customize browser for tests
+        ChromeOptions chromeOptions = new ChromeOptions();
+        //to ignore "Your connection is not private issue"
+        chromeOptions.setAcceptInsecureCerts(true);
+        //provide chromeOptions object into chromedriver constructor
+        driver = new ChromeDriver(chromeOptions);
         driver.get("http://practice.cybertekschool.com/");
         driver.manage().window().maximize();
     }
