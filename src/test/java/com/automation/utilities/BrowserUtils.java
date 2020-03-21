a092d64d01f39b44247d3febcc41d228e709b69c
ffilizz <filizikibas@gmail.com>
2020-03-21 16:59:22 -0500

    WEbTables practice

--- src/test/java/com/automation/utilities/BrowserUtils.java
+++ src/test/java/com/automation/utilities/BrowserUtils.java
@@ -1,5 +1,10 @@
 package com.automation.utilities;
 
+import org.openqa.selenium.WebElement;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class BrowserUtils {
 
     public static void wait(int second) {
@@ -9,5 +14,17 @@ public class BrowserUtils {
             e.printStackTrace();
         }
 
+    }
+
+    /**
+     * @param elements represents collection of WebElements
+     * @return collection of strings
+     */
+    public static List<String> getTextFromWebElements(List<WebElement> elements) {
+        List<String> textValues = new ArrayList<>();
+        for (WebElement element : elements) {
+            textValues.add(element.getText());
+        }
+        return textValues;
     }
 }
