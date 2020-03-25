fce153b49d3ce7dc7a17bc638259699313aec02d
ffilizz <filizikibas@gmail.com>
2020-03-24 19:38:29 -0500

    Explicit wait

---
+++ src/test/java/com/automation/tests/day12/ConfigurationReaderTest.java
@@ -0,0 +1,14 @@
+package com.automation.tests.day12;
+
+import com.automation.utilities.ConfigurationReader;
+import org.testng.annotations.Test;
+
+public class ConfigurationReaderTest {
+
+    @Test
+    public void readProperties(){
+        String browser = ConfigurationReader.getProperty("browser");
+        System.out.println(browser);
+    }
+}
+
