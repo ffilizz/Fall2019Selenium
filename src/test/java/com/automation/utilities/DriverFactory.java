779e8934663bc4eb6f5c1b9ee44cfa9eca1150be
ffilizz <filizikibas@gmail.com>
2020-04-05 01:31:12 -0500

    Day 16 Screenshot and Report

--- src/test/java/com/automation/utilities/DriverFactory.java
+++ src/test/java/com/automation/utilities/DriverFactory.java
@@ -1,38 +1,21 @@
 package com.automation.utilities;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.PageLoadStrategy;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.ie.InternetExplorerDriver;
-import org.openqa.selenium.opera.OperaDriver;
 
 public class DriverFactory {
-    /**
-     * This method return webdriver object based on browser type
-     * If you want to use chrome browser, just provide chrome as a parameter
-     * @param browserName
-     * @return webdriver object
-     */
 
-    public static WebDriver createDriver (String browserName){
-
-       switch(browserName.toLowerCase()){
-           case "chrome":
-               WebDriverManager.chromedriver().version("79.0").setup();
-               return new ChromeDriver();
-           case "opera":
-               WebDriverManager.operadriver().setup();
-               return new OperaDriver();
-           case "firefox":
-               WebDriverManager.firefoxdriver().setup();
-               return new FirefoxDriver();
-           case"ie":
-               WebDriverManager.iedriver().setup();
-               return new InternetExplorerDriver();
-
-       }
-       return null;
+    public static WebDriver createDriver(String browserName) {
+        if (browserName.equalsIgnoreCase("chrome")) {
+            WebDriverManager.chromedriver().version("79.0").setup();
+            return new ChromeDriver();
+        } else {
+            WebDriverManager.firefoxdriver().setup();
+            return new FirefoxDriver();
+        }
     }
-
 }
